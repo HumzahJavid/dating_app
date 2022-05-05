cc77c68eff6ebf245884e796f01f1266bb27a59c
HumzahJavid <HumzahJavid@users.noreply.github.com>
2022-05-06 00:27:49 +0100

    SEPP-28 Add pydantic models (input and response) for chat creation
    To show in openapi docs

--- dating_app/api/chat/chat.py
+++ dating_app/api/chat/chat.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 
-from fastapi import APIRouter, Body, Request, status
+from fastapi import APIRouter, Request, status
 from fastapi.templating import Jinja2Templates
+
+from dating_app.schemas.ChatModel import InitiateChat, InitiateChatResponse
 
 BASE_PATH = Path(__file__).resolve().parent
 templates = Jinja2Templates(directory=BASE_PATH / "templates")
@@ -9,8 +11,8 @@ templates = Jinja2Templates(directory=BASE_PATH / "templates")
 router = APIRouter()
 
 
-def create_chat_session_id(id1, id2):
-    id_list = [id1, id2]
+def create_chat_session_id(fields: InitiateChat):
+    id_list = [fields.user_initiating_id, fields.user_receiving_id]
     id_list.sort()
     return id_list[0] + "-" + id_list[1]
 
@@ -20,9 +22,13 @@ async def chat_page(request: Request):
     return templates.TemplateResponse("chat.html", {"request": request})
 
 
-@router.post("/initiate_chat", status_code=status.HTTP_201_CREATED)
-async def initiate_chat(user_initiating_id=Body(...), user_receiving_id=Body(...)):
+@router.post(
+    "/initiate_chat",
+    status_code=status.HTTP_201_CREATED,
+    response_model=InitiateChatResponse,
+)
+async def initiate_chat(fields: InitiateChat):
     print("initiating chat?")
-    chat_session_id = create_chat_session_id(user_initiating_id, user_receiving_id)
-    print("returning", chat_session_id)
-    return chat_session_id
+    print(fields)
+    chat_session_id = create_chat_session_id(fields)
+    return InitiateChatResponse(chat_session_id=chat_session_id)
